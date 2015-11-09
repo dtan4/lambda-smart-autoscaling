3cd7f278d909cdea42cdf44a32e20538dbd11735
Daisuke Fujita <dtanshi45@gmail.com>
2015-11-09 15:28:35 +0900

    Use Promise

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-var AWS = require('aws-sdk')
+var AWS = require('aws-sdk');
+var Promise = require('bluebird');
+
+function asyncDescribeInstances(ec2, params) {
+  return new Promise(function(resolve, reject) {
+    return ec2.describeInstances(params, function(err, data) {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(data);
+    });
+  });
+}
 
 exports.handler = function(event, context) {
   var ec2 = new AWS.EC2();
 
-  ec2.describeInstances({}, function(err, data) {
-    if (err) {
-      console.log(err, err.stack);
-      context.fail('[FAIL]');
-    } else {
+  asyncDescribeInstances(ec2, {})
+    .then(function(data) {
       data.Reservations.forEach(function(reservation) {
         reservation.Instances.forEach(function(instance) {
           console.log(instance.InstanceId);
         });
       });
-      context.succeed('[SUCCESS]')
-    }
-  })
+
+      context.succeed('[SUCCESS]');
+    })
+    .catch(function(err) {
+      console.log(err, err.trace);
+      context.fail('[FAIL]');
+    });
 }
